11f9249a2b1986a57861606fda39ce9dc760107d
plastictortoise <simplebinary@yahoo.com>
2023-02-13 22:03:53 +0000

    Update gh-pages to output generated at bb6dcbf

---
+++ _app/immutable/chunks/2-f635716f.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-5f2d819c.js";export{t as component};
---
+++ _app/immutable/chunks/3-1b76730d.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/about/_page.svelte-1c9ba46d.js";export{t as component};
